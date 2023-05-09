914d5bd6499f4abb87491f6beeeb1e0a624ca244
XyoZang <z1304242002@163.com>
2023-05-09 11:14:50 +0800

    注册功能重写
    适应新数据库

--- regist.php
+++ regist.php
@@ -1,13 +1,3 @@
-<!doctype html>
-
-<html lang="cn">
-<head>
-    <meta charset="UTF-8">
-    <title>登陆中···</title>
-    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@10'></script>
-    <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-<body>
 <?php
 
 $config = include 'config.php';
@@ -21,95 +11,82 @@ $mysqlDbname = $config['mysqlDbname'];
 // 创建与 MySQL 数据库的连接
 $conn = new mysqli($mysqlHost, $mysqlUsername, $mysqlPassword, $mysqlDbname);
 
+function generateUuid()
+{
+    if (function_exists('uuid_create')) {
+        $uuid = uuid_create(1); // 使用 UUID v1 版本生成
+    } else {
+        // 如果 uuid_create() 不存在，使用其他方式生成
+        $data = openssl_random_pseudo_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // 指定 UUID 版本为 4
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // 标记为 UUID 变体
+        $uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
+    return $uuid;
+}
  // Check if connection is successful
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
-    echo "404 DataNotFound!";
+    $status = "Failed";
+    $info = "服务器连接失败！";
 } else{
     // Insert data into database
     $userName = $_POST['userName'];
-    $userID = $_POST['userID'];
-    $userGender = $_POST['userGender'];
-    $userPhone = $_POST['userPhone'];
-    $userRelaShip = $_POST['userRelaShip'];
     $userPswd = $_POST['userPswd'];
-    $patientName = $_POST['patientName'];
-    $patientGender = $_POST['patientGender'];
-    $patientAge = $_POST['patientAge'];
-    $patientID = $_POST['patientID'];
-    $patientPhone = $_POST['patientPhone'];
+    $userEmail = $_POST['userEmail'];
     // 执行 SQL 查询语句，获取所有已录入的家属和被监护者的身份证号
-    $result = $conn->query("SELECT `被监测人身份证号`, `家属身份证号` FROM `亲属关系`");
+    $result1 = $conn->query("SELECT user_name, email FROM user_account");
     // 存储所有已录入的家属和被监护者的身份证号
-    $IDfst = array();
-    $IDsec = array();
-    while ($row = $result->fetch_assoc()) {
-        $IDfst[] = $row["家属身份证号"];
-        $IDsec[] = $row["被监测人身份证号"];
+    $IDlist = array();
+    $Emaillist = array();
+    while ($row = $result1->fetch_assoc()) {
+        $IDlist[] = $row["user_name"];
+        $Emaillist = $row["email"];
     }
    
     // 判断用户输入的信息是否已经被录入
-    if (in_array($_POST["userID"], $IDfst) && in_array($_POST["patientID"], $IDsec)) {
-    ?>
-    <script>
-        Swal.fire({
-            title: '注册失败！',
-            text: "该账号已被创建！",
-            icon: 'warning',
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: '点击重试'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                window.location.href="Test.html?p=Regist";
-            }
-        })
-    </script>
-    <?php
-    } 
-    else {
-        // 如果没有被录入，则将该信息插入到数据库中
-        $sql1 = "INSERT INTO `家属` (`姓名`,`身份证号`,`性别`,`电话号码`) VALUES ('$userName','$userID','$userGender','$userPhone')";
-        $sql2 = "INSERT INTO `亲属关系` (`被监测人身份证号`,`家属身份证号`,`血缘关系`,`登录密码`) VALUES ('$patientID','$userID','$userRelaShip','$userPswd')";
-        $sql3 = "INSERT INTO `被监测人` (`姓名`,`身份证号`,`性别`,`年龄`,`联系电话`) VALUES ('$patientName','$patientID','$patientGender','$patientAge','$patientPhone')";
-        // 执行查询
-        $conn->query($sql1);
-        $conn->query($sql3);
-        $conn->query($sql2);
-        // Display success message
-    ?>
-    <script>
-        let timerInterval
-        Swal.fire({
-            title: '注册成功!',
-            html: '即将跳转至登录界面： <b></b> milliseconds.',
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: () => {
-                Swal.showLoading()
-                const b = Swal.getHtmlContainer().querySelector('b')
-                timerInterval = setInterval(() => {
-                b.textContent = Swal.getTimerLeft()
-                }, 100)
-            },
-            willClose: () => {
-                clearInterval(timerInterval)
-            }
-        }).then((result) => {
-            /* Read more about handling dismissals below */
-            if (result.dismiss === Swal.DismissReason.timer) {
-                console.log('I was closed by the timer');
-            }
-            window.location.href="Test.html?p=Login";
-        })
-    </script>
-    <?php
+    if (in_array($userName, $IDlist)) {
+        $status = "Failed";
+        $info = "该用户名已被注册！";
+    } elseif (in_array($userEmail, $Emaillist)) {
+        $status = "Failed";
+        $info = "该邮箱已被注册！";
     }
-
- // Close database connection
+    else {
+        //用户密码加密
+        // $options = [
+        //     'cost' => 12,
+        // ];
+        // $hashPswd =  password_hash($userPswd, PASSWORD_BCRYPT, $options);
+        // 如果没有被录入，则将该信息插入到数据库中
+        $uid = generateUuid();
+        $uid = md5($uid);
+        $now_time = date("Y-m-d H:i:s");
+        $sql1 = "INSERT INTO user_account (uid, user_name, password, email, create_time) VALUES ('$uid','$userName','$userPswd','$userEmail', '$now_time')";
+        // 执行查询
+        $result = $conn->query($sql1);
+        if($result === false) {
+            //查询失败
+            $status = "Failed";
+            $info = "数据插入失败！";
+            $log = "Error: " . mysqli_error($conn);
+         } else {
+            //查询成功
+            $status = "Success";
+            $info = "注册成功！";
+            $log =  "Number of rows: " . mysqli_affected_rows($conn);
+         }
+    }
 }
-
+// Close database connection
 mysqli_close($conn);
+//返回用户注册状态信息给ajax
+$response = array(
+    'status' => $status,
+    'message' => $info,
+    "log" => $log
+);
+echo json_encode($response);
 ?>
-</body>
 
