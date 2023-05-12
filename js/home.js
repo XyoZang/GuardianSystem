bea6ba84e20cedb9fbddbb2e9592eb562e9c19aa
XyoZang <z1304242002@163.com>
2023-05-13 01:07:49 +0800

    添加编辑验证
    略微优化显示

--- js/home.js
+++ js/home.js
@@ -1,4 +1,3 @@
-$(".edit_profile").hide();
 $(document).ready(function(){
     //查询用户账户信息
     $.ajax({
@@ -22,27 +21,63 @@ $(document).ready(function(){
     $("#btn_edit_profile").click(function(){
         $(".show_profile").hide();
         $(".edit_profile").show();
+        $("#btn_edit_profile").hide();
         $("#btn_profile_submit_cancel").show();
+        //编辑时附带原本信息
+        $("#edit_name").val($("#name").html());
+        $("#edit_gender").val($("#gender").html());
+        $("#edit_phone_number").val($("#phone_number").html());
+        $("#edit_id_number").val($("#id_number").html());
     });
     $("#btn_cancel_profile").click(function(){
         $(".edit_profile").hide();
         $("#btn_profile_submit_cancel").hide();
         $(".show_profile").show();
+        $("#btn_edit_profile").show();
     })
-    // ^1[3456789]\d{9}$ 这个正则表达式可以匹配以1开头，第二位为3-9之间的数字，后面跟着9个数字的手机号码。
     $("#btn_submit_profile").click(function(){
-        $.ajax({
-            url: '/php/editUserProfile.php',
-            method: 'POST',
-            data: $('#formEditProfile').serialize(),
-            dataType: 'json',
-            success: function(response) {
-                // 处理服务器返回的数据
-                console.log(response);
-            },
-            error: function(xhr, status, error) {
-                console.log('Ajax连接失败');
-            }
-        });
+        if (phoneNumCheck() && idNumCheck()){
+            $.ajax({
+                url: '/php/editUserProfile.php',
+                method: 'POST',
+                data: $('#formEditProfile').serialize(),
+                dataType: 'json',
+                success: function(response) {
+                    // 处理服务器返回的数据
+                    console.log(response);
+                },
+                error: function(xhr, status, error) {
+                    console.log('Ajax连接失败');
+                }
+            });
+        } else{
+            console.log("请检查输入信息！");
+        }
     })
 });
+//校验手机号
+function phoneNumCheck(){
+    var text= $("#edit_phone_number").val();
+    //这个正则表达式可以匹配以1开头，第二位为3-9之间的数字，后面跟着9个数字的手机号码。
+    var re = /^1[3456789]\d{9}$/;
+    if(re.test(text) || !text) {
+        $("#phoneNumCheckSpan").html("");
+        return true;
+    }else {
+        $("#phoneNumCheckSpan").html("手机号格式不正确！");
+        return false;
+    }
+}
+//校验身份证号, 由于身份证号可不填，要求text为空时同样可以通过
+function idNumCheck(){
+    var text= $("#edit_id_number").val();
+    var re18 = /^([1-6][1-9]|50)\d{4}(18|19|20)\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/;
+    var re15 =  /^([1-6][1-9]|50)\d{4}\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\d{3}$/;
+    if(re18.test(text) || re15.test(text) || !text) {
+        $("#idNumCheckSpan").html("");
+        return true;
+    } else {
+        $("#idNumCheckSpan").html("身份证号格式不正确！");
+        return false;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-main();
-//主函数
-function main(){
-    if ($.cookie('token')){
-        console.log("已登录");
-        $(".loginFalse").hide();
-    } else{
-        $(".loginTrue").hide();
-    }
+if ($.cookie('token')){
+    console.log("已登录");
+    $(".loginTrue").show();
+} else{
+    $(".loginFalse").show();
 }
 function logOut(){
     if ($.removeCookie('token', {path: '/'})){
